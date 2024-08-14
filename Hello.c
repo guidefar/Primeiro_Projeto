7fa74c141f5d8707e0493b6980c91d7507a475bd
guidefar <guilhermee.alvesdf@gmail.com>
2024-08-13 21:04:58 -0300

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main (){
+    printf("CARACTERES ESPECIAIS\n\n\\n => Nova linha\n\\t => tabulacao\n\\\\ => escrever\\\n\\\"\\\' => Escapar aspas");
+}
---
+++ soma.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int numero1 = 20, numero2 = 5;
+    int soma;
+    
+    soma = numero1 + numero2;
+    
+    printf("A soma dos numeros e de:%d", soma);
+
+}
